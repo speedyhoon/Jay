5d3c8b0517a0784de1c3ad26410755f2e7179637
sh <16303791+speedyhoon@users.noreply.github.com>
2024-12-07 16:11:41 +1100

    Remove unused functions.

--- byte.go
+++ byte.go
@@ -7,14 +7,6 @@ func WriteBytes(y []byte, s []byte, length int) {
 	}
 }
 
-func WriteBytesAt(y []byte, s []byte, length, at int) int {
-	if length != 0 {
-		y[0] = byte(length) // Set how long the []byte is.
-		copy(y[strSizeOf:], s)
-	}
-	return at + length + strSizeOf
-}
-
 func ReadBytes(b []byte) (h []byte, size int, _ bool) {
 	size = int(b[0]) + strSizeOf
 	if size == strSizeOf || len(b) < size {
@@ -22,22 +14,4 @@ func ReadBytes(b []byte) (h []byte, size int, _ bool) {
 	}
 
 	return b[strSizeOf:size], size, true
-}
-
-func ReadBytesPtrErr(b []byte, h *[]byte) error {
-	size := int(b[0]) + strSizeOf
-	if size == strSizeOf {
-		return nil
-	}
-	if len(b) < size {
-		return ErrUnexpectedEOB
-	}
-
-	*h = b[strSizeOf:size]
-	return nil
-}
-
-func ReadBytesAt(b []byte, at int) (h []byte, size int, ok bool) {
-	h, size, ok = ReadBytes(b)
-	return h, at + size, ok
 }
