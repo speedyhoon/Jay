6461e75e5df97b5e92c2ae02ddc22f33de4dd0e9
sh <16303791+speedyhoon@users.noreply.github.com>
2025-01-30 12:10:40 +1100

    Add StringsSize8, StringsSize16 and StringsSize24 for future []string support.

--- string.go
+++ string.go
@@ -1,5 +1,46 @@
 package jay
 
+// StringsSize8 calculates the total size needed to represent a []string and
+// returns the quantity of strings in a slice of strings
+// StringsSize8 only supports up to 255 items.
+func StringsSize8(s []string) (total, qty int, sizes []byte) {
+	qty = len(s)
+	sizes = make([]byte, qty)
+	var l int
+	for i := range s {
+		l = len(s[i])
+		sizes[i] = byte(l)
+		total += l
+	}
+	return
+}
+
+// StringsSize16 only supports up to 65,535 items.
+func StringsSize16(s []string) (total, qty int, sizes []byte) {
+	qty = len(s)
+	sizes = make([]byte, qty*2)
+	var l int
+	for i := range s {
+		l = len(s[i])
+		sizes[i*2], sizes[i*2+1] = byte(l), byte(l>>8)
+		total += l
+	}
+	return
+}
+
+// StringsSize24 only supports up to 16,777,215 items.
+func StringsSize24(s []string) (total, qty int, sizes []byte) {
+	qty = len(s)
+	sizes = make([]byte, qty*3)
+	var l int
+	for i := range s {
+		l = len(s[i])
+		sizes[i*3], sizes[i*3+1], sizes[i*3+2] = byte(l), byte(l>>8), byte(l>>16)
+		total += l
+	}
+	return
+}
+
 //const strSizeOf = 1
 
 func WriteStrings(ln, y []byte, s []string) {
--- string_test.go
+++ string_test.go
@@ -1,6 +1,7 @@
 package jay_test
 
 import (
+	"fmt"
 	"github.com/speedyhoon/jay"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -19,4 +20,61 @@ func TestStrings(t *testing.T) {
 	//assert.Equal(t, list, s)
 	//assert.Equal(t, length, l)
 	//assert.True(t, ok)
+}
+
+func TestStringsSize8(t *testing.T) {
+	tests := []struct {
+		wantTotal int
+		wantQty   int
+		wantSizes []byte
+		s         []string
+	}{
+		{wantTotal: 16, wantQty: 3, wantSizes: []byte{3, 3, 10}, s: []string{"one", "two", "hello 😄"}},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("test[%d] total:%d size:%d", i, tt.wantTotal, tt.wantQty), func(t *testing.T) {
+			gotTotal, gotQty, gotSizes := jay.StringsSize8(tt.s)
+			assert.Equalf(t, tt.wantTotal, gotTotal, "StringsSize8(%v)", tt.s)
+			assert.Equalf(t, tt.wantQty, gotQty, "StringsSize8(%v)", tt.s)
+			assert.Equalf(t, tt.wantSizes, gotSizes, "StringsSize8(%v)", tt.s)
+		})
+	}
+}
+
+func TestStringsSize16(t *testing.T) {
+	tests := []struct {
+		wantTotal int
+		wantQty   int
+		wantSizes []byte
+		s         []string
+	}{
+		{wantTotal: 16, wantQty: 3, wantSizes: []byte{3, 0, 3, 0, 10, 0}, s: []string{"one", "two", "hello 😄"}},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("test[%d] total:%d size:%d", i, tt.wantTotal, tt.wantQty), func(t *testing.T) {
+			gotTotal, gotQty, gotSizes := jay.StringsSize16(tt.s)
+			assert.Equalf(t, tt.wantTotal, gotTotal, "StringsSize16(%v)", tt.s)
+			assert.Equalf(t, tt.wantQty, gotQty, "StringsSize16(%v)", tt.s)
+			assert.Equalf(t, tt.wantSizes, gotSizes, "StringsSize16(%v)", tt.s)
+		})
+	}
+}
+
+func TestStringsSize24(t *testing.T) {
+	tests := []struct {
+		wantTotal int
+		wantQty   int
+		wantSizes []byte
+		s         []string
+	}{
+		{wantTotal: 16, wantQty: 3, wantSizes: []byte{3, 0, 0, 3, 0, 0, 10, 0, 0}, s: []string{"one", "two", "hello 😄"}},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("test[%d] total:%d size:%d", i, tt.wantTotal, tt.wantQty), func(t *testing.T) {
+			gotTotal, gotQty, gotSizes := jay.StringsSize24(tt.s)
+			assert.Equalf(t, tt.wantTotal, gotTotal, "StringsSize24(%v)", tt.s)
+			assert.Equalf(t, tt.wantQty, gotQty, "StringsSize24(%v)", tt.s)
+			assert.Equalf(t, tt.wantSizes, gotSizes, "StringsSize24(%v)", tt.s)
+		})
+	}
 }
