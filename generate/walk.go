5f8037c11932789543648b3bcb7e9e1215a06533
sh <16303791+speedyhoon@users.noreply.github.com>
2025-02-14 15:42:07 +1100

    Improve comments.

--- generate/walk.go
+++ generate/walk.go
@@ -22,15 +22,15 @@ type field struct {
 
 	aliasType  string // Alias name assigned to the type, for example: `type Toggle bool`, field.typ = "bool", field.aliasType = "Toggle".
 	arraySize  int    // 0 = not an array or slice, -1 = slice, >=1 = array size.
-	elmSize    uint   // Quantity of bytes required to represent the type.
-	pkgReq     string // Which package is required to be imported if referenced in the generated code.
+	elmSize    uint   // Quantity of bytes required to represent the type, for example: bool = 0, int8 = 1, uint16 = 2, int64 = 8, etc.
+	pkgReq     string // The third party package required to be imported if referenced in the generated code.
 	arrayType  string // The type without the size in brackets. An empty string is not an array.
 	lenVar     string // For variable length types, this is the name of the variable that stores the length from len(b[X]) in marshal methods.
 	tag        string // The tag value within `j:""`
 	tagOptions        // Valid tag options that have been successfully parsed and loaded from the `tag` string.
-	isFixedLen bool   // Is represented by a fixed quantity of bytes (like int64) or a variable quantity of bytes (like string & slices).
+	isFixedLen bool   // Is represented by a fixed quantity of bytes (like int64) or a variable quantity of bytes (like string and slices).
 	isDef      bool   // Whether the type is a new definition (type num int) verses an alias (type num = int) or a normal type (int).
-	isFirst    bool
+	isFirst    bool   // True when this field is the first to be marshalled and unmarshalled in the struct methods (not the first defined in the struct definition).
 	isLast     bool
 }
 type fieldList []*field
