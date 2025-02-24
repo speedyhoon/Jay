6e444864b58351684cff76c0d3a0c06ebfe363d9
sh <16303791+speedyhoon@users.noreply.github.com>
2025-02-25 02:20:45 +1100

    Fix broken tests. Add tests for jay.ReadBools8() & jay.SizeBools8().

--- string_test.go
+++ string_test.go
@@ -29,14 +29,14 @@ func TestStringsSize8(t *testing.T) {
 		wantSizes []byte
 		s         []string
 	}{
-		{wantTotal: 16, wantQty: 3, wantSizes: []byte{3, 3, 10}, s: []string{"one", "two", "hello 😄"}},
+		{wantTotal: 20, wantQty: 3, wantSizes: []byte{3, 3, 10}, s: []string{"one", "two", "hello 😄"}},
 	}
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("test[%d] total:%d size:%d", i, tt.wantTotal, tt.wantQty), func(t *testing.T) {
-			gotTotal, gotQty, gotSizes := jay.StringsSize8(tt.s)
+			gotTotal /*, gotQty, gotSizes*/ := jay.StringsSize8(tt.s)
 			assert.Equalf(t, tt.wantTotal, gotTotal, "StringsSize8(%v)", tt.s)
-			assert.Equalf(t, tt.wantQty, gotQty, "StringsSize8(%v)", tt.s)
-			assert.Equalf(t, tt.wantSizes, gotSizes, "StringsSize8(%v)", tt.s)
+			//assert.Equalf(t, tt.wantQty, gotQty, "StringsSize8(%v)", tt.s)
+			//assert.Equalf(t, tt.wantSizes, gotSizes, "StringsSize8(%v)", tt.s)
 		})
 	}
 }
@@ -48,19 +48,19 @@ func TestStringsSize16(t *testing.T) {
 		wantSizes []byte
 		s         []string
 	}{
-		{wantTotal: 16, wantQty: 3, wantSizes: []byte{3, 0, 3, 0, 10, 0}, s: []string{"one", "two", "hello 😄"}},
+		{wantTotal: 24, wantQty: 3, wantSizes: []byte{3, 0, 3, 0, 10, 0}, s: []string{"one", "two", "hello 😄"}},
 	}
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("test[%d] total:%d size:%d", i, tt.wantTotal, tt.wantQty), func(t *testing.T) {
-			gotTotal, gotQty, gotSizes := jay.StringsSize16(tt.s)
+			gotTotal /*, gotQty, gotSizes*/ := jay.StringsSize16(tt.s)
 			assert.Equalf(t, tt.wantTotal, gotTotal, "StringsSize16(%v)", tt.s)
-			assert.Equalf(t, tt.wantQty, gotQty, "StringsSize16(%v)", tt.s)
-			assert.Equalf(t, tt.wantSizes, gotSizes, "StringsSize16(%v)", tt.s)
+			//assert.Equalf(t, tt.wantQty, gotQty, "StringsSize16(%v)", tt.s)
+			//assert.Equalf(t, tt.wantSizes, gotSizes, "StringsSize16(%v)", tt.s)
 		})
 	}
 }
 
-func TestStringsSize24(t *testing.T) {
+/*func TestStringsSize24(t *testing.T) {
 	tests := []struct {
 		wantTotal int
 		wantQty   int
@@ -77,4 +77,4 @@ func TestStringsSize24(t *testing.T) {
 			assert.Equalf(t, tt.wantSizes, gotSizes, "StringsSize24(%v)", tt.s)
 		})
 	}
-}
+}*/
