73feeb5507a1da8a8a4dcea38b53d905e5114a67
sh <16303791+speedyhoon@users.noreply.github.com>
2025-07-03 03:59:27 +1000

    Add functions to convert []byte to and from types []complex64 and []complex128.

--- complex.go
+++ complex.go
@@ -17,3 +17,39 @@ func WriteComplex128(y []byte, c complex128) {
 func ReadComplex128(y []byte) complex128 {
 	return complex(ReadFloat64(y[:_8]), ReadFloat64(y[_8:_16]))
 }
+
+func WriteComplex64s(y []byte, slice []complex64) {
+	for i := range slice {
+		WriteComplex64(y[i*_8:i*_8+_8], slice[i])
+	}
+}
+
+func ReadComplex64s(y []byte, length int) (t []complex64) {
+	if length == 0 {
+		return
+	}
+
+	t = make([]complex64, length)
+	for i := 0; i < length; i++ {
+		t[i] = ReadComplex64(y[i*_8 : i*_8+_8])
+	}
+	return
+}
+
+func WriteComplex128s(y []byte, slice []complex128) {
+	for i := range slice {
+		WriteComplex128(y[i*_16:i*_16+_16], slice[i])
+	}
+}
+
+func ReadComplex128s(y []byte, length int) (t []complex128) {
+	if length == 0 {
+		return
+	}
+
+	t = make([]complex128, length)
+	for i := 0; i < length; i++ {
+		t[i] = ReadComplex128(y[i*_16 : i*_16+_16])
+	}
+	return
+}
--- complex_test.go
+++ complex_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/speedyhoon/jay"
 	"github.com/speedyhoon/rando"
+	"github.com/speedyhoon/utl/tf"
 	"github.com/stretchr/testify/assert"
+	"math"
 	"testing"
 )
 
@@ -29,5 +31,49 @@ func TestComplex128(t *testing.T) {
 			assert.Equal(t, c, jay.ReadComplex128(b))
 		})
 		c = rando.Complex128()
+	}
+}
+
+func TestRoundTripComplex64s(t *testing.T) {
+	var b []byte
+	var list []complex64
+	t.Run("zero", func(t *testing.T) {
+		jay.WriteComplex64s(b, list)
+		assert.Equal(t, list, jay.ReadComplex64s(b, 0))
+	})
+
+	list = make([]complex64, math.MaxUint8)
+	for i := range list {
+		list[i] = rando.Complex64()
+	}
+
+	for i := 1; i <= math.MaxUint8; i++ {
+		tf.Run(t, i, func(t *testing.T) {
+			b = make([]byte, i*8)
+			jay.WriteComplex64s(b, list[:i])
+			assert.Equal(t, list[:i], jay.ReadComplex64s(b, i))
+		})
+	}
+}
+
+func TestRoundTripComplex128s(t *testing.T) {
+	var b []byte
+	var list []complex128
+	t.Run("zero", func(t *testing.T) {
+		jay.WriteComplex128s(b, list)
+		assert.Equal(t, list, jay.ReadComplex128s(b, 0))
+	})
+
+	list = make([]complex128, math.MaxUint8)
+	for i := range list {
+		list[i] = rando.Complex128()
+	}
+
+	for i := 1; i <= math.MaxUint8; i++ {
+		tf.Run(t, i, func(t *testing.T) {
+			b = make([]byte, i*16)
+			jay.WriteComplex128s(b, list[:i])
+			assert.Equal(t, list[:i], jay.ReadComplex128s(b, i))
+		})
 	}
 }
