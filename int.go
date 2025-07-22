71948768da86827ce32ecdfc3f1657b9c2f1156f
sh <16303791+speedyhoon@users.noreply.github.com>
2025-07-22 20:23:35 +1000

    Fix comments.

--- int.go
+++ int.go
@@ -6,54 +6,54 @@ const _24, _40, _48, _56 = 24, 40, 48, 56
 // Index positions.
 const _0, _3, _5, _6, _7 = 0, 3, 5, 6, 7
 
-// ReadInt64 converts the first 8 bits in `b` into an int64.
+// ReadInt64 converts the first 8 bytes in `y` into an int64.
 func ReadInt64(y []byte) int64 {
 	return int64(y[_0]) | int64(y[_1])<<_8 | int64(y[_2])<<_16 | int64(y[_3])<<_24 |
 		int64(y[_4])<<_32 | int64(y[_5])<<_40 | int64(y[_6])<<_48 | int64(y[_7])<<_56
 }
 
-// ReadIntX64 converts the first 8 bits in `b` into an int.
+// ReadIntX64 converts the first 8 bytes in `y` into an int.
 func ReadIntX64(y []byte) int {
 	return int(y[_0]) | int(y[_1])<<_8 | int(y[_2])<<_16 | int(y[_3])<<_24 |
 		int(y[_4])<<_32 | int(y[_5])<<_40 | int(y[_6])<<_48 | int(y[_7])<<_56
 }
 
-// ReadIntX32 converts the first 6 bits in `b` into an int.
+// ReadIntX32 converts the first 6 bytes in `y` into an int.
 func ReadIntX32(y []byte) int {
 	return int(int32(y[_0]) | int32(y[_1])<<_8 | int32(y[_2])<<_16 | int32(y[_3])<<_24)
 }
 
-// ReadInt32 converts the first 4 bits in `b` into an int32.
+// ReadInt32 converts the first 4 bytes in `y` into an int32.
 func ReadInt32(y []byte) int32 {
 	return int32(y[_0]) | int32(y[_1])<<_8 | int32(y[_2])<<_16 | int32(y[_3])<<_24
 }
 
-// ReadInt16 converts the first 2 bits in `b` into an int16.
+// ReadInt16 converts the first 2 bytes in `y` into an int16.
 func ReadInt16(y []byte) int16 {
 	return int16(y[_0]) | int16(y[_1])<<_8
 }
 
-// WriteInt64 sets the first 8 bits in `b` to the value of `i`.
+// WriteInt64 sets the first 8 bytes in `y` to the value of `i`.
 func WriteInt64(y []byte, i int64) {
 	y[_0], y[_1], y[_2], y[_3], y[_4], y[_5], y[_6], y[_7] = byte(i), byte(i>>_8), byte(i>>_16), byte(i>>_24), byte(i>>_32), byte(i>>_40), byte(i>>_48), byte(i>>_56)
 }
 
-// WriteIntX64 sets the first 8 bits in `b` to the value of `i`.
+// WriteIntX64 sets the first 8 bytes in `y` to the value of `i`.
 func WriteIntX64(y []byte, i int) {
 	y[_0], y[_1], y[_2], y[_3], y[_4], y[_5], y[_6], y[_7] = byte(i), byte(i>>_8), byte(i>>_16), byte(i>>_24), byte(i>>_32), byte(i>>_40), byte(i>>_48), byte(i>>_56)
 }
 
-// WriteIntX32 sets the first 4 bits in `b` to the value of `i`.
+// WriteIntX32 sets the first 4 bytes in `y` to the value of `i`.
 func WriteIntX32(y []byte, i int) {
 	y[_0], y[_1], y[_2], y[_3] = byte(i), byte(i>>_8), byte(i>>_16), byte(i>>_24)
 }
 
-// WriteInt32 sets the first 4 bits in `b` to the value of `i`.
+// WriteInt32 sets the first 4 bytes in `y` to the value of `i`.
 func WriteInt32(y []byte, i int32) {
 	y[_0], y[_1], y[_2], y[_3] = byte(i), byte(i>>_8), byte(i>>_16), byte(i>>_24)
 }
 
-// WriteInt16 sets the first 2 bits in `b` to the value of `i`.
+// WriteInt16 sets the first 2 bytes in `y` to the value of `i`.
 func WriteInt16(y []byte, i int16) {
 	y[_0], y[_1] = byte(i), byte(i>>_8)
 }
