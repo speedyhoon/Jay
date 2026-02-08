0bc86e23452f042e9da1da8a239efc2d166e1430
sh <16303791+speedyhoon@users.noreply.github.com>
2026-02-08 11:59:49 +1100

    Add tests for embedded structs and types.

---
+++ generate/testdata/embedded/jay_test.go
@@ -0,0 +1,61 @@
+package embedded
+
+import (
+	"github.com/speedyhoon/rando"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestFuzz_Configuration(t *testing.T) {
+	var expected, actual Configuration
+	require.NoError(t, actual.UnmarshalJ(expected.MarshalJ()))
+	require.Equal(t, expected, actual)
+	require.Equal(t, Configuration{}, expected)
+	require.Equal(t, Configuration{}, actual)
+
+	expected = Configuration{
+		Alarm: Alarm{
+			SirenOn:     rando.Duration(),
+			SirenOff:    rando.Duration(),
+			Repeats:     rando.Uint8(),
+			CheckEvery:  rando.Duration(),
+			Types:       rando.Bytes(),
+			Attempts:    rando.Uint8(),
+			KeyAttempts: rando.Uint8(),
+		},
+		DataLog: DataLog{
+			Version:     rando.Uint(),
+			VersionRace: rando.Uint(),
+			FileSizeMax: rando.Uint32(),
+			Cluster:     rando.Bytes(),
+			Off:         rando.Bool(),
+		},
+		Types: Types{
+			UniMog:  UniMog(rando.Uint8()),
+			Cluster: rando.Bytes(),
+		},
+	}
+	src := expected.MarshalJ()
+	require.NoError(t, actual.UnmarshalJ(src))
+	// require.NotEqual(t, Configuration{}, expected)
+	// require.NotEqual(t, Configuration{}, actual)
+	require.Equal(t, expected, actual)
+}
+
+func TestFuzz_Types(t *testing.T) {
+	var expected, actual Types
+	require.NoError(t, actual.UnmarshalJ(expected.MarshalJ()))
+	require.Equal(t, expected, actual)
+	require.Equal(t, Types{}, expected)
+	require.Equal(t, Types{}, actual)
+
+	expected = Types{
+		UniMog:  UniMog(rando.Uint8()),
+		Cluster: rando.Bytes(),
+	}
+	src := expected.MarshalJ()
+	require.NoError(t, actual.UnmarshalJ(src))
+	// require.NotEqual(t, Types{}, expected)
+	// require.NotEqual(t, Types{}, actual)
+	require.Equal(t, expected, actual)
+}
---
+++ generate/testdata/embedded/pkg.go
@@ -0,0 +1,42 @@
+package embedded
+
+import "time"
+
+// Alarm properties
+type Alarm struct {
+	SirenOn         time.Duration
+	SirenOff        time.Duration
+	Repeats         uint8
+	CheckEvery      time.Duration
+	Types           Cluster
+	Attempts        uint8
+	AttemptsFunc    func()
+	KeyAttempts     uint8
+	KeyAttemptsFunc func()
+}
+
+type Configuration struct {
+	Alarm   Alarm
+	DataLog DataLog
+	Types
+}
+
+// DataLog properties for data logging files.
+type DataLog struct {
+	Version     uint
+	VersionRace uint
+	FileSizeMax uint32
+	Cluster
+	Off bool
+}
+
+// Embedded types
+type (
+	UniMog  uint8
+	Cluster []byte
+)
+
+type Types struct {
+	UniMog
+	Cluster
+}
