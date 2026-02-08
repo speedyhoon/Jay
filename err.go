d82da9fd8cff067be74ff13b8b9e66d3135e7ad0
sh <16303791+speedyhoon@users.noreply.github.com>
2026-02-08 11:34:17 +1100

    Extend exported error description.

--- err.go
+++ err.go
@@ -4,4 +4,6 @@ import "errors"
 
 // ErrUnexpectedEOB indicates the end of the byte slice buffer was unexpectedly encountered
 // while deserializing a fixed-size block, resulting in an incomplete result.
+// Either the overall byte slice length is incorrect,
+// or the length specified for one or more fields is incorrect.
 var ErrUnexpectedEOB = errors.New("unexpected EOB")
